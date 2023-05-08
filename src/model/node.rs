9b6524eb8d52537bada31c3130eb276106b69c21
OwenFeik <owenhfeik@gmail.com>
2023-05-08 17:01:10 +1000

    improve text parsing

--- src/model/node.rs
+++ src/model/node.rs
@@ -3,7 +3,7 @@ pub enum Style {
     Bold,
     Italic,
     Strikethrough,
-    Underline
+    Underline,
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -11,12 +11,40 @@ pub enum Node {
     Style(Style, Vec<Node>),
     Document(Vec<Node>),
     Heading(Vec<Node>),
-    Italic(Vec<Node>),
+    Item(Vec<Node>),
+    List(Vec<Node>),
     Text(String),
 }
 
 impl Node {
     pub fn text(text: &str) -> Self {
         Self::Text(text.trim().to_string())
+    }
+
+    pub fn add_text(&mut self, text: &str) {
+        match self {
+            Node::Style(_, children)
+            | Node::Document(children)
+            | Node::Heading(children)
+            | Node::Item(children)
+            | Node::List(children) => children.push(Node::text(text)),
+            Node::Text(string) => {
+                if !string.is_empty() {
+                    string.push(' ');
+                }
+                string.push_str(text.trim());
+            }
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        match self {
+            Node::Style(_, children)
+            | Node::Document(children)
+            | Node::Heading(children)
+            | Node::Item(children)
+            | Node::List(children) => !children.iter().any(|n| !n.is_empty()),
+            Node::Text(string) => string.trim().is_empty(),
+        }
     }
 }
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -4,6 +4,8 @@ use std::str::pattern::Pattern;
 
 use crate::model::node::{Node, Style};
 
+const CONTROL: &str = "#*\n";
+
 fn drop_n<'a>(input: &'a str, n: usize) -> &'a str {
     if input.len() <= n {
         ""
@@ -52,70 +54,85 @@ fn consume_chars<'a>(input: &'a str, chars: &str) -> (&'a str, &'a str) {
 fn parse_heading<'a>(input: &'a str) -> (&'a str, Node) {
     let (rest, _) = consume_chars(input, "#");
     let (rest, text) = consume_line(rest);
-    (rest, Node::Heading(parse(text)))
+    (rest, Node::Heading(parse(text, false)))
 }
 
 fn parse_style<'a>(input: &'a str) -> (&'a str, Node) {
     match input {
         input if input.starts_with("**") => {
             let (rest, text) = consume(drop_n(input, 2), "**");
-            let nodes = parse(text);
+            let nodes = parse(text, false);
             (drop_n(rest, 2), Node::Style(Style::Bold, nodes))
-        },
+        }
         input if input.starts_with('*') => {
             let (rest, text) = consume(drop_first(input), '*');
-            (drop_first(rest), Node::Style(Style::Italic, parse(text)))
+            (drop_first(rest), Node::Style(Style::Italic, parse(text, false)))
         }
-        _ => ("", Node::Document(vec![]))
+        _ => ("", Node::Document(vec![])),
     }
 }
 
-fn parse(input: &str) -> Vec<Node> {
+fn parse_text<'a>(input: &'a str) -> (&'a str, Vec<Node>) {
+    let mut node = Node::Text(String::new());
     let mut nodes = Vec::new();
     let mut rest = input;
-    let mut node = String::new();
-    while !rest.is_empty() {
+    while {
         let text;
-        (rest, text) = consume_until_any(rest, "*#\n");
-        
-        if !node.is_empty() {
-            node.push('\n');
+        (rest, text) = consume_until_any(rest, CONTROL);
+
+        // Empty line, new text element.
+        if text.trim().is_empty() && !node.is_empty() {
+            nodes.push(node);
+            node = Node::Text(String::new());
         }
-        node.push_str(text.trim());
+        node.add_text(text);
 
-        if rest.starts_with('#') {
-            if !node.is_empty() {
-                nodes.push(Node::text(&node));
-                node = String::new();
-            }
-
-            let heading;
-            (rest, heading) = parse_heading(rest);
-            nodes.push(heading);
-        } else if rest.starts_with('*') {
-            if !node.is_empty() {
-                nodes.push(Node::text(&node));
-                node = String::new();
-            }
-
-            let styled;
-            (rest, styled) = parse_style(rest);
-            nodes.push(styled);
-        }
-
-        rest = consume_whitespace(rest);
-    }
-
+        let text;
+        (rest, text) = consume_chars(rest, "\n");
+        !text.is_empty()
+    } {}
 
     if !node.is_empty() {
-        nodes.push(Node::text(&node));
+        nodes.push(node);
+    }
+
+    (rest, nodes)
+}
+
+fn parse_list(input: &str) {
+    
+}
+
+fn parse(input: &str, at_line_start: bool) -> Vec<Node> {
+    let mut nodes = Vec::new();
+    let mut rest = input;
+    while !rest.is_empty() {
+        match rest.chars().nth(0) {
+            None => break,
+            Some('#') => {
+                let heading;
+                (rest, heading) = parse_heading(rest);
+                nodes.push(heading);
+            }
+            Some('*') if at_line_start => {}
+            Some('*') => {
+                    let styled;
+                (rest, styled) = parse_style(rest);
+                nodes.push(styled);
+            }
+            _ => {
+                let text;
+                (rest, text) = parse_text(rest);
+                nodes.extend(text)
+            }
+        }
     }
 
     nodes
 }
 
 fn parse_document(input: &str) -> Node {
-    Node::Document(parse(input))
+    Node::Document(parse(input, true))
 }
 
 #[cfg(test)]
@@ -146,7 +163,7 @@ mod test {
             Node::Document(vec![
                 Node::text("some text"),
                 Node::Heading(vec![Node::text("Heading one")]),
-                Node::text("Some other text\nmore text")
+                Node::text("Some other text more text")
             ])
         )
     }
@@ -167,13 +184,47 @@ mod test {
     fn test_parse_bold_italic() {
         assert_eq!(
             super::parse_document("# Normal *italic* **bold** *italic* normal"),
+            Node::Document(vec![Node::Heading(vec![
+                Node::text("Normal"),
+                Node::Style(Style::Italic, vec![Node::text("italic")]),
+                Node::Style(Style::Bold, vec![Node::text("bold")]),
+                Node::Style(Style::Italic, vec![Node::text("italic")]),
+                Node::text("normal")
+            ])])
+        )
+    }
+
+    #[test]
+    fn test_line_break() {
+        assert_eq!(
+            super::parse_document("line\nno break\n  \nbroken"),
+            Node::Document(vec![Node::text("line no break"), Node::text("broken")])
+        )
+    }
+
+    #[test]
+    fn test_list() {
+        assert_eq!(
+            super::parse_document(
+                r#"
+                    # Heading
+                    * List item
+                        * Sub list item
+                        * Another sub item
+                    * Another list item
+                "#
+            ),
             Node::Document(vec![
-                Node::Heading(vec![
-                    Node::text("Normal"),
-                    Node::Style(Style::Italic, vec![Node::text("italic")]),
-                    Node::Style(Style::Bold, vec![Node::text("bold")]),
-                    Node::Style(Style::Italic, vec![Node::text("italic")]),
-                    Node::text("normal")
+                Node::Heading(vec![Node::text("Heading")]),
+                Node::List(vec![
+                    Node::Item(vec![
+                        Node::text("List item"),
+                        Node::List(vec![
+                            Node::Item(vec![Node::text("Sub list item")]),
+                            Node::Item(vec![Node::text("Another sub item")])
+                        ]),
+                    ]),
+                    Node::Item(vec![Node::text("Another sub item")])
                 ])
             ])
         )
