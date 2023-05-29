a623f303fa0d79c2bfeec4f151753ad37e8c22b2
OwenFeik <owenhfeik@gmail.com>
2023-05-29 12:47:09 +1000

    start on rendering directories

---
+++ src/log.rs
@@ -0,0 +1,41 @@
+use std::fmt::Display;
+
+enum Level {
+    Debug,
+    Info,
+    Warning,
+    Error
+}
+
+impl Display for Level {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let string = match self {
+            Self::Debug => "DEBUG",
+            Self::Info => "INFO",
+            Self::Warning => "WARN",
+            Self::Error => "ERROR"
+        };
+        
+        f.write_str(&format!("[{: <5}]", string))
+    }
+}
+
+fn log(level: Level, msg: &str) {
+    println!("{level} {msg}");
+}
+
+pub fn debug(msg: &str) {
+    log(Level::Debug, msg);
+}
+
+pub fn info(msg: &str) {
+    log(Level::Info, msg);
+}
+
+pub fn warning(msg: &str) {
+    log(Level::Warning, msg);
+}
+
+pub fn error(msg: &str) {
+    log(Level::Error, msg);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![feature(pattern)]
 
+use std::path::{Path, PathBuf};
+
+mod log;
 mod model;
 mod parse;
 mod render;
@@ -7,20 +10,60 @@ mod render;
 #[cfg(test)]
 mod test;
 
-fn main() {
-    let file = std::env::args().nth(1).expect(
-        "Usage: mdwiki file.md"
-    );
+fn fail(msg: &str) -> ! {
+    log::error(msg);
+    panic!();
+}
 
-    let markdown = std::fs::read_to_string(&file).expect(
-        "Failed to read file"
-    );
+fn process_file(file: &Path, outdir: &Path) {
+    let Some(Some(name)) = file.file_name().map(std::ffi::OsStr::to_str) else {
+        log::error(
+            &format!("Couldn't find file name for file: {}", file.display())
+        );
+        return;
+    };
+
+    let Ok(markdown) = std::fs::read_to_string(file) else {
+        log::error(&format!("Failed to read input file: {}", file.display()));
+        return;
+    };
 
     let document = parse::parse_document(&markdown);
     let html = render::render_document(&document);
 
-    let output = file.replace(".md", ".html");
-    std::fs::write(output, html).expect(
-        "Failed to write file"
-    );
+    let output = outdir.join(name.replace(".md", ".html"));
+    if std::fs::write(&output, html).is_ok() {
+        log::info(&format!("Rendered {} to {}", file.display(), output.display()));
+    } else {
+        log::error(&format!("Failed to write file: {}", output.display()));
+    }
+}
+
+fn process_directory(indir: &Path, outdir: &Path) {
+
+}
+
+fn main() {
+    let Some(arg) = std::env::args().nth(1) else {
+        fail("Usage: mdwiki file.md");
+    };
+
+    let Ok(metadata) = std::fs::metadata(&arg) else {
+        fail("Couldn't read argument file metadata")
+    };
+
+    if metadata.is_file() {
+        let path = PathBuf::from(arg);
+        let Some(parent) = path.parent() else {
+            fail("Couldn't find parent directory of input file.");
+        };
+        process_file(&path, parent);
+    } else if metadata.is_dir() {
+        let path = PathBuf::from(arg);
+        let indir = path.clone();
+        let Some(outdir) = &indir.parent().map(|p| p.join("build")) else {
+            fail("Couldn't choose an output directory for files.");
+        };
+        process_directory(&indir, outdir);
+    }
 }
